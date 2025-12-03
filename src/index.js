24d174221673077b2d9805296de295b2aa2681a4
Anonymous-99-web <chiemeriejoseph090@gmail.com>
2025-12-03 10:53:49 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./App.css"; // global styles (already provided earlier)
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
